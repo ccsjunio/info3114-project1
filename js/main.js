7e15115aba8b96810aecfd51d4beeb46f6e0d134
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-02-09 19:04:58 -0500

    establish retrieval of values for year, month and day selected
    handle case on which any of the elements to be selected is not selected

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ window.addEventListener("load",()=>{
             }
             let option = document.createElement("option");
             option.setAttribute("selected","selected");
+            option.value = "";
             option.innerHTML = "Choose the month";
             select.appendChild(option);
             option.removeAttribute("selected");
@@ -35,6 +36,7 @@ window.addEventListener("load",()=>{
             }
             let option = document.createElement("option");
             option.setAttribute("selected","selected");
+            option.value = "";
             option.innerHTML = "Choose the day";
             select.appendChild(option);
             option.removeAttribute("selected");
@@ -48,5 +50,51 @@ window.addEventListener("load",()=>{
     );//end of day select for each
 
     // add a listener for the retrieve data button
+    let retrievalForm = document.querySelector("form#retrieval-form");
+    retrievalForm.addEventListener("submit",handleRetrieveDataRequest);
+
+    function handleRetrieveDataRequest(event){
+        console.log("caught event on handleRetrieveDataRequest - event = ", event);
+        event.preventDefault();
+        //assign year selector to a variable
+        let year = document.getElementById("selectYearToRetrieve");
+        //assign month selector to a variable
+        let month = document.getElementById("selectMonthToRetrieve");
+        //assign day selector to a variable
+        let day = document.getElementById("selectDayToRetrieve");
+        
+        //test if any of them are empty
+        if(year.value == "" || month.value == "" || day.value == ""){
+            //throw an error message to the user and do not retrieve any data
+            let messageTitle = "Form empty";
+            let message = "You must choose all three options: year, month and day!";
+            showUserMessageModal(messageTitle,message);
+        }
+
+    }
+
+    function showUserMessageModal(title,message){
+        let options = {
+            "keyboard":true,
+            "focus":true,
+            "show":true
+        };
+        document.getElementById("modal-title").innerHTML = title;
+        document.getElementById("modal-body").innerHTML = message;
+
+        document.getElementById("btn-modal-trigger").click();
+    
+    }
+
+    //TODO: develop a custom modal trigger function
+    function Modal(title,message){
+        this.title = title;
+        this.message = message;
+        this.windowWidth = window.innerWidth;
+        this.windowHeight = window.innerHeight;
+        this.width = "326px";
+        this.height = "24px";
+
+    }
     
 });
